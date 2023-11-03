dd88bfc865130d988d8ccf285043840caf4b54d2
Sarah Murdock <sg.mdoc7@gmail.com>
2023-11-03 01:36:57 -0400

    navbar

---
+++ app/web/src/theme.ts
@@ -0,0 +1,13 @@
+const theme = {
+  fonts: {
+    primary: "'DM Sans', sans-serif",
+  },
+  colors: {
+    white: '#ffffff',
+    black: '#2C2F24',
+    primary: '#9B2D2B',
+    light: '#FFF4E1',
+  },
+};
+
+export default theme;
