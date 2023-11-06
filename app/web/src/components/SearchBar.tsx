b55612c2eb034fe700d7a81f48b4e0e3872ebc78
Sarah Murdock <sg.mdoc7@gmail.com>
2023-11-06 17:42:05 -0500

    icon

--- app/web/src/components/SearchBar.tsx
+++ app/web/src/components/SearchBar.tsx
@@ -4,24 +4,49 @@ import styled from "styled-components";
 import { Link } from "react-router-dom";
 import theme from "../theme";
 
-const SearchBarContainer = styled.nav`
+const SearchBarContainer = styled.div`
+  // Changed from nav to div if it's not specifically for navigation
   background: ${theme.colors.white};
   backdrop-filter: blur(5px);
   display: flex;
-  justify-content: center; /* Changed from space-between to center */
-  padding: 1rem 2rem;
   align-items: center;
   width: 55%;
-  margin: 0 auto; /* This will center the container itself if it's not full width */
+  margin: 0 auto;
+  position: relative; // Needed for absolute positioning of the search icon
+`;
+
+const InputWrapper = styled.div`
+  width: 100%; // Take full width of the SearchBarContainer
+  position: relative; // Relative position to place the search icon inside it
 `;
 
 const Input = styled.input`
-  padding: 0.5rem;
+  padding: 0.5rem 2.5rem 0.5rem 1rem; // Right padding to make room for the icon
   border: 1px solid ${theme.colors.grey};
-  border-radius: 4px;
+  border-radius: 72px;
   font-size: 1rem;
-  width: 100%; /* Make the input take the full width of its parent */
-  display: block; /* This can help if there are other inline elements inside the container */
+  width: 100%;
+`;
+
+const SearchIcon = styled.div`
+  position: absolute;
+  top: 50%;
+  right: 1rem;
+  transform: translateY(-60%);
+  cursor: pointer;
+
+  svg {
+    fill: ${theme.colors.grey};
+    transition: fill 0.3s ease;
+    width: 18px; // Smaller width
+    height: 18px; // Smaller height
+  }
+
+  &:hover {
+    svg {
+      fill: ${theme.colors.grey}; // Adjust the color on hover if needed
+    }
+  }
 `;
 
 const SearchBar: React.FC<{ onSearch: (searchTerm: string) => void }> = ({
@@ -36,13 +61,26 @@ const SearchBar: React.FC<{ onSearch: (searchTerm: string) => void }> = ({
 
   return (
     <SearchBarContainer>
-      <Input
-        type="text"
-        placeholder="Search "
-        value={searchTerm}
-        onChange={handleInputChange}
-      />
-      {/* Add other navigation or UI elements here */}
+      <InputWrapper>
+        <Input
+          type="text"
+          placeholder="Search"
+          value={searchTerm}
+          onChange={handleInputChange}
+        />
+        <SearchIcon>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            fill="currentColor"
+            className="bi bi-search"
+            viewBox="0 0 16 16"
+          >
+            <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
+          </svg>
+        </SearchIcon>
+      </InputWrapper>
     </SearchBarContainer>
   );
 };
