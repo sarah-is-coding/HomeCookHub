0a90e99115f48ce2c5108c3b045307c9dbb27971
Johnathan Thompson <Thomp5je@mail.uc.edu>
2023-11-07 21:08:45 -0500

    Authentication Email Password

--- app/web/src/auth/src/config.tsx
+++
@@ -1,9 +0,0 @@
-export const firebaseApp = {
-  apiKey: "AIzaSyDGgxcZliGnzZa_K2PZ4sc5bdrOjPAu5Ro",
-  authDomain: "homecookhub-0.firebaseapp.com",
-  projectId: "homecookhub-0",
-  storageBucket: "homecookhub-0.appspot.com",
-  messagingSenderId: "454367080500",
-  appId: "1:454367080500:web:214319e1e98d2405021a61",
-  measurementId: "G-FNCPSCNPP8"
-};
