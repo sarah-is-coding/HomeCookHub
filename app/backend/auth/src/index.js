cc5590c5b4e44164735814d28858d012a3d20712
Johnathan Thompson <Thomp5je@mail.uc.edu>
2023-11-13 21:13:58 -0500

    Improvements to auth

--- app/backend/auth/src/index.js
+++ app/backend/auth/src/index.js
@@ -1,24 +1,24 @@
-import "./styles.css";
-import {
-  hideLoginError,
-  showLoginState,
-  showLoginForm,
-  showApp,
-  showLoginError,
+import './styles.css';
+import { 
+  hideLoginError, 
+  showLoginState, 
+  showLoginForm, 
+  showApp, 
+  showLoginError, 
   btnLogin,
   btnSignup,
-  btnLogout,
-} from "./ui";
+  btnLogout
+} from './ui'
 
-import { initializeApp } from "firebase/app";
-import {
+import { initializeApp } from 'firebase/app';
+import { 
   getAuth,
-  onAuthStateChanged,
+  onAuthStateChanged, 
   signOut,
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
-  connectAuthEmulator,
-} from "firebase/auth";
+  connectAuthEmulator
+} from 'firebase/auth';
 
 const firebaseApp = initializeApp({
   apiKey: "AIzaSyDGgxcZliGnzZa_K2PZ4sc5bdrOjPAu5Ro",
@@ -32,50 +32,53 @@ const firebaseApp = initializeApp({
 
 // Login using email/password
 const loginEmailPassword = async () => {
-  const loginEmail = txtEmail.value;
-  const loginPassword = txtPassword.value;
+  const loginEmail = txtEmail.value
+  const loginPassword = txtPassword.value
 
-  await signInWithEmailAndPassword(auth, loginEmail, loginPassword);
-};
+  await signInWithEmailAndPassword(auth, loginEmail, loginPassword)
+
+}
 
 // Create new account using email/password
 const createAccount = async () => {
-  const email = txtEmail.value;
-  const password = txtPassword.value;
+  const email = txtEmail.value
+  const password = txtPassword.value
 
   try {
-    await createUserWithEmailAndPassword(auth, email, password);
-  } catch (error) {
-    console.log(`There was an error: ${error}`);
-    showLoginError(error);
+    await createUserWithEmailAndPassword(auth, email, password)
   }
-};
+  catch(error) {
+    console.log(`There was an error: ${error}`)
+    showLoginError(error)
+  } 
+}
 
 // Monitor auth state
 const monitorAuthState = async () => {
-  onAuthStateChanged(auth, (user) => {
+  onAuthStateChanged(auth, user => {
     if (user) {
-      console.log(user);
-      showApp();
-      showLoginState(user);
+      console.log(user)
+      showApp()
+      showLoginState(user)
 
-      hideLoginError();
-      hideLinkError();
-    } else {
-      showLoginForm();
-      lblAuthState.innerHTML = `You're not logged in.`;
+      hideLoginError()
+      hideLinkError()
     }
-  });
-};
+    else {
+      showLoginForm()
+      lblAuthState.innerHTML = `You're not logged in.`
+    }
+  })
+}
 
 // Log out
 const logout = async () => {
   await signOut(auth);
-};
+}
 
-btnLogin.addEventListener("click", loginEmailPassword);
-btnSignup.addEventListener("click", createAccount);
-btnLogout.addEventListener("click", logout);
+btnLogin.addEventListener("click", loginEmailPassword) 
+btnSignup.addEventListener("click", createAccount)
+btnLogout.addEventListener("click", logout)
 
 const auth = getAuth(firebaseApp);
 connectAuthEmulator(auth, "http://localhost:9099");
