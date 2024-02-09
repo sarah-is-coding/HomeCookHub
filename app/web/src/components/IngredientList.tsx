7d07a958206ecf85e436f58de39ebc6bb4c1afc3
mmeineke1014 <35080951+mmeineke1014@users.noreply.github.com>
2024-02-09 11:14:51 -0500

    Updated Recipe Card to display ingredients and instructions for the recipe

---
+++ app/web/src/components/IngredientList.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+interface IngredientListProps {
+    ingredients: Object;
+    quantities: Object;
+    units: Object;
+}
+
+const IngredientList: React.FC<IngredientListProps> = (props) => {
+    const ingredientMap = new Map(Object.entries(props.ingredients))
+    const quantityMap = new Map(Object.entries(props.quantities))
+    const unitMap = new Map(Object.entries(props.units))
+
+    return(
+    <div>
+        <p><b>Ingredient List:</b></p>
+        <ul>
+            {
+                Array.from(Object.keys(props.ingredients)).map((key: string) => {
+                return(<li key={key}>{quantityMap.get(key)} {unitMap.get(key)} {ingredientMap.get(key)}</li>)
+            })}
+        </ul>
+    </div>
+    );
+};
+
+export default IngredientList;
